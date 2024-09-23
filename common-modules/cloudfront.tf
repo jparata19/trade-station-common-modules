75c45cd38f7527a40db224f1c6a8dfe5c65feeed
Jose Pablo Arata <josepablo191290@gmail.com>
2024-09-23 11:17:09 -0600

    change cloudfront

--- common-modules/cloudfront.tf
+++ common-modules/cloudfront.tf
@@ -25,7 +25,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   logging_config {
     include_cookies = false
-    bucket          = "cloudfront-logs.s3.amazonaws.com"
+    bucket          = "cloudfront-logs-trade-station.s3.amazonaws.com"
     prefix          = "cloudfront-logs"
   }
 
